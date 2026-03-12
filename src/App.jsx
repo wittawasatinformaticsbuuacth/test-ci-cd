1471c41a271332f827e73022f57e9541605dbf7b
wittawasatinformaticsbuuacth <wittawas@informatics.buu.ac.th>
2026-03-12 09:14:52 +0700

    fix: update greeting in App component to reflect version change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello CI/CD 2</h1>
+      <h1>Hello CI/CD 3</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
